7389d08885afd3e94339309730456805df6e598b
ramyarao242 <ramyarao242@gmail.com>
2025-05-02 15:06:58 -0400

    Fixed port main.py

--- main.py
+++ main.py
@@ -15,3 +15,7 @@ def verse_random():
 @app.get("/verse/ask")
 def verse_ask(question: str = Query(..., description="Your question to Chanakya")):
     return ask_chanakya(question)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=10000)
