0e58fe4f3f23e3c93f04720a64fdec0d598940db
ramyarao242 <ramyarao242@gmail.com>
2025-05-05 14:36:02 -0400

    Verse_loader to use embeddings

--- app/verse_loader.py
+++ app/verse_loader.py
@@ -2,7 +2,7 @@ import json
 from pathlib import Path
 
 def load_verses():
-    path = Path("app/data/chanakya_neeti_all_chapters.json")
+    path = Path("app/data/chanakya_verses_with_embeddings.json")
     with path.open(encoding="utf-8") as f:
         return json.load(f)
 
